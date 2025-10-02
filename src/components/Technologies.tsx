ce0ca3073be52938b8643749be8d2a641925fa83
SAMCOM-07 <shondesamuel07@gmail.com>
2025-10-02 21:40:58 +0100

    updated

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Card from './Card'
 
 const Technologies = () => {
   return (
     <div className='conpad mt-18'>
       <h1 className='mb-8'>Technologies & Tools</h1>
-      
+            <Card></Card>
     </div>
   )
 }
