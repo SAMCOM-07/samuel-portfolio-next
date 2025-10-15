c4da953731f19a994b296e686eb8d0ff9d2b2ec8
Samuel Shonde <samuelshonde07@gmail.com>
2025-10-15 16:37:58 +0100

    updated

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -11,8 +11,8 @@ const Technologies = () => {
         <div className="flex items-center gap-6 animate-scroll-horizontal py-4">
           {
             stackData && stackData.map((stack, index) =>
-              <div className="shadow-[0_0_5px] shadow-foreground h-fit w-fit rounded-md animate-pulse">
-                <Card key={index}>
+              <div key={index} className="shadow-[0_0_5px] shadow-foreground h-fit w-fit rounded-md animate-pulse">
+                <Card>
                   <div className="w-[100px] h-[150px] overflow-hidden">
                     <Image src={stack.img} alt="stack.name" width={100} height={100} className="w-full h-[70%] object-contain mx-auto bg-foreground/10 p-1 rounded-md" />
                     <h2 className="mt-4 text-center">{stack.name}</h2>
