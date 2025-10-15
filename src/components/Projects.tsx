d545323c7c5de9f16329552d9aa1b04e3fb49571
Samuel Shonde <samuelshonde07@gmail.com>
2025-10-15 16:56:40 +0100

    updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,9 +3,10 @@ import React from "react";
 const Projects = () => {
   return (
     <div className="projectbg w-full h-[700px] relative mt-22 pt-16 conpad">
+      {/* background */}
+      <div className="projectbg w-full -z-20 absolute inset-0"></div>
       <h1 className="text-center">Featured Projects</h1>
       <p className='text-center mx-auto text-muted-foreground mt-4 max-w-lg'>A collection of projects that showcase my skills and passion for creating innovative digital solutions.</p>
-      <div className="projectbg w-full -z-20 absolute inset-0"></div>
     </div>
   );
 };
